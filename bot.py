d78a6608927101b98c639bcee91371bdd86807ec
KousekiMiyo <kouseki.miyo@gmail.com>
2022-07-20 12:42:32 +0900

    bug fix

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import subprocess
 import discord
 from secrets import TOKEN
@@ -10,18 +11,29 @@ from datetime import datetime
 # python3 -m pip install -U "discord.py"
 
 
-def get_volatage():
+def get_voltage():
     i2c = board.I2C()
-    ch0 = adafruit_ina260.INA260(i2c,address=0x42) # battery
-    v0 = ch0.voltage
-    ch1 = adafruit_ina260.INA260(i2c,address=0x4c) # pannel     
-    v1 = ch1.voltage
-    ch2 = adafruit_ina260.INA260(i2c,address=0x43) # load
-    v2 = ch2.voltage
-    return v0,v1,v2
+    try:
+        ch0 = adafruit_ina260.INA260(i2c,address=0x42) # solar
+        v0,i0,p0 = ch0.voltage,ch0.current/1000,ch0.power/1000
+    except:
+        v0,i0,p0 = 0,0,0
+    try:
+        ch1 = adafruit_ina260.INA260(i2c,address=0x4c) # load     
+        v1,i1,p1 = ch1.voltage,ch1.current/1000,ch1.power/1000
+    except:
+        v0,i0,p0 = 0,0,0
+    try:
+        ch2 = adafruit_ina260.INA260(i2c,address=0x43) # battery    
+        v2,i2,p2 = ch2.voltage,ch2.current/1000,ch2.power/1000
+    except:
+        v0,i0,p0 = 0,0,0
+        
+    if i2>0:
+        p2 = p2*-1
+    return v0,v1,v2,i0,i1,i2,p0,p1,p2        
 
 def main():
-
     _temp2 = 0
     _cputemp = 0
     _pw_health = 'none'
@@ -54,7 +66,7 @@ def main():
                 temp2,humid2 = sens2.read_temp_and_humid()
                 now = datetime.today()
                 now_str = now.strftime('%Y-%m-%d %H:%M:%S')
-                txt = now_str +'\n'
+                txt = now_str +'\n'                
                 txt += " - 筐体温度['C]: {:>3.2f}\n".format(temp2)
                 cmd = 'bash check.sh | grep temp'
                 res = subprocess.run(cmd,shell=True,
@@ -68,16 +80,21 @@ def main():
                                      stdout=subprocess.PIPE)
                 clock = float(res.stdout.split('=')[1])
                 txt += ' - クロック[Hz]: {:>3.2e}\n'.format(clock)
+                cmd = "df -h | grep root | awk '{print $5}'"
+                res = subprocess.run(cmd,shell=True,
+                                     encoding='utf-8',
+                                     stdout=subprocess.PIPE)                
+                txt += " - SDカード使用率: {:>s}".format(res.stdout)                
                 cmd = 'bash check.sh | grep throttled'
                 res = subprocess.run(cmd,shell=True,
                                      encoding='utf-8',
                                      stdout=subprocess.PIPE)
                 pw_health = res.stdout.split('=')[1].replace('\n','')
                 txt += ' - 電源診断: {:>s}\n'.format(pw_health)
-                v0,v1,v2 = get_volatage()
-                txt += " - バッテリー[V]: {:>5.2f}\n".format(v0)
-                txt += " - 太陽電池[V]: {:>5.2f}\n".format(v1)
-                txt += " - 負荷[V]: {:>5.2f}\n".format(v2)
+                v0,v1,v2,i0,i1,i2,p0,p1,p2 = get_voltage()
+                txt += " - 太陽電池[W]: {:>5.2f}\n".format(p0)
+                txt += " - 負荷[W]: {:>5.2f}\n".format(p1)
+                txt += " - バッテリー[W]: {:>5.2f}\n".format(p2)
                 _temp2 = temp2
                 _cputemp = cputemp
                 _pw_health = pw_health
--- main_soracom.py
+++ main_soracom.py
@@ -4,16 +4,41 @@ import subprocess
 import board
 import adafruit_ina260
 from sensors import Adt7410,Hdc1000
+from datetime import datetime
+
+# def get_voltage():
+#     i2c = board.I2C()
+#     ch0 = adafruit_ina260.INA260(i2c,address=0x42) # solar
+#     v0,i0,p0 = ch0.voltage,ch0.current/1000,ch0.power/1000
+#     ch1 = adafruit_ina260.INA260(i2c,address=0x4c) # load     
+#     v1,i1,p1 = ch1.voltage,ch1.current/1000,ch1.power/1000
+#     ch2 = adafruit_ina260.INA260(i2c,address=0x43) # battery    
+#     v2,i2,p2 = ch2.voltage,ch2.current/1000,ch2.power/1000
+#     if i2>0:
+#         p2 = p2*-1
+#     return v0,v1,v2,i0,i1,i2,p0,p1,p2
 
 def get_voltage():
     i2c = board.I2C()
-    ch0 = adafruit_ina260.INA260(i2c,address=0x42) # battery
-    v0,i0,p0 = ch0.voltage,ch0.current,ch0.power
-    ch1 = adafruit_ina260.INA260(i2c,address=0x4c) # pannel     
-    v1,i1,p1 = ch1.voltage,ch1.current,ch1.power    
-    ch2 = adafruit_ina260.INA260(i2c,address=0x43) # load
-    v2,i2,p2 = ch2.voltage,ch2.current,ch2.power        
-    return v0,v1,v2,i0,i1,i2,p0,p1,p2
+    try:
+        ch0 = adafruit_ina260.INA260(i2c,address=0x42) # solar
+        v0,i0,p0 = ch0.voltage,ch0.current/1000,ch0.power/1000
+    except:
+        v0,i0,p0 = 0,0,0
+    try:
+        ch1 = adafruit_ina260.INA260(i2c,address=0x4c) # load     
+        v1,i1,p1 = ch1.voltage,ch1.current/1000,ch1.power/1000
+    except:
+        v1,i1,p1 = 0,0,0
+    try:
+        ch2 = adafruit_ina260.INA260(i2c,address=0x43) # battery    
+        v2,i2,p2 = ch2.voltage,ch2.current/1000,ch2.power/1000
+    except:
+        v0,i0,p0 = 0,0,0
+        
+    if i2>0:
+        p2 = p2*-1
+    return v0,v1,v2,i0,i1,i2,p0,p1,p2        
 
 def get_health():
     cmd = 'bash check.sh | grep temp'
@@ -48,17 +73,18 @@ if __name__ == "__main__":
         'box_temp':box_temp,
         'cpu_temp':cputemp,
         'clock':clock,
-        'battery_voltage':v0,
-        'solar_voltage':v1,
-        'load_voltage':v2,
-        'battery_current':i0,
-        'solar_current':i1,
-        'load_current':i2,
-        'battery_power':p0,
-        'solar_power':p1,
-        'load_power':p2,        
+        'solar_voltage':v0,
+        'load_voltage':v1,
+        'battery_voltage':v2,
+        'solar_current':i0,
+        'load_current':i1,
+        'battery_current':i2,
+        'solar_power':p0,
+        'load_power':p1,
+        'battery_power':p2,        
     }    
     res = requests.post('http://harvest.soracom.io',
                         data=json.dumps(data),
                         headers={'Content-Type': 'application/json'})
-    print(res,cputemp,box_temp)
+    now = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+    print(now,res,cputemp,box_temp)
